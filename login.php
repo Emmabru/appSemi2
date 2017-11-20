523db3d401dfecd323279b3dbef6a50764f3fdb2
Emmabru <emmabru@kth.se>
2017-11-20 01:53:52 +0100

    kan posta komentarer men kan inte få sessions med anv-namn att funka. det poppar inte upp

--- login.php
+++ login.php
@@ -11,28 +11,24 @@
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
       $sql = "SELECT username FROM user WHERE username = '$myusername' and password = '$mypassword'";
-      // Test of sql query
-      //echo $sql;
-
-
-			/*Send error message to the server log if error connecting to the database
-			if (!mysqli_connect("localhost","bad_user","bad_password","my_db")) {
-			    error_log("Failed to connect to database!", 0);
-			} */
+      
       $result = mysqli_query($db,$sql);
       
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $username = $row['username'];
       
+
       $count = mysqli_num_rows($result);
       
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
       if($count == 1) {
          //session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
+
+         $_SESSION['username'] = $username;
+			
          //$_SESSION['username'] = $username;
-         header("location: index.html");
+         header("location: recipe.html");
       }else {
          echo "Your Login Name or Password is invalid";
          sleep(10);
